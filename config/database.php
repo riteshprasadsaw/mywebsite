59bf4d004fc24b8e2101fa8f9e10d8edb4079527
Ritesh Saw <Ritesh Saw>
2017-03-10 21:39:05 +0530

    switch database'

--- config/database.php
+++ config/database.php
@@ -41,8 +41,8 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '3306'),
+            'host' => env('DB_HOST', ''),
+            'port' => env('DB_PORT', ''),
             'database' => env('DB_DATABASE', ''),
             'username' => env('DB_USERNAME', ''),
             'password' => env('DB_PASSWORD', ''),
