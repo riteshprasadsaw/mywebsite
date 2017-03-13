56860329119018ff1bd7e4d525233396c365eae8
Ritesh Saw <Ritesh Saw>
2017-03-13 08:54:23 +0530

    added welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -73,44 +73,7 @@
             }
         </style>
 </head>
-   <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login1'))
-                <div class="top-right links">
-                    @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ url('/login') }}">Login</a>
-                        <a href="{{ url('/register') }}">Register</a>
-                    @endif
-                </div>
-            @endif
-
-
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-</head>
+  
    <body>
         <div class="flex-center position-ref full-height">
             @if (Route::has('login1'))
